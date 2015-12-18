71520ae36a417bec24dae69297f9e0625c240ff8
1kastner <1kastner@informatik.uni-hamburg.de>
2015-12-18 16:38:22 +0200

    start colors

--- merge_all.sh
+++ merge_all.sh
@@ -1,2 +1,7 @@
 #bash script
-cat `find *[^bibliography].bibtex`  > bibliography.bibtex
+
+#compile
+cat `find *[^bibliography].bibtex`  > bibliography.bib
+
+#and distribute
+cp E:/uni/bibliography/bibliography.bib E:/uni/metu/MemProcs/petrinetze/projectproposal/
