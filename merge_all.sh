c09f71d8c6b515d12dd4a71703afb2010282ab5d
1kastner <1kastner@informatik.uni-hamburg.de>
2016-01-06 17:11:16 +0200

    Rename from bibtex to bib to correspond to conventions.

--- merge_all.sh
+++ merge_all.sh
@@ -1,7 +1,8 @@
 #bash script
 
 #compile
-cat `find *[^bibliography].bibtex`  > bibliography.bib
+cat `find *[^bibliography].bib`  > bibliography.bib
 
 #and distribute
 cp E:/uni/bibliography/bibliography.bib E:/uni/metu/MemProcs/petrinetze/projectproposal/
+cp E:/uni/bibliography/bibliography.bib E:/uni/metu/MemProcs/petrinetze/project\ proposal\ presentation/
